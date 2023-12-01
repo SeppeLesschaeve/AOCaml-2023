d62b2b3895e558b1befc7ed9cb62a4c727287669
Seppe Lesschaeve <seppe.lesschaeve@student.kuleuven.be>
2023-12-01 17:34:25 +0100

    Refactoring of day 1

--- day01/day1.ml
+++ day01/day1.ml
@@ -2,39 +2,31 @@ let lines =
   let contents = In_channel.with_open_bin "day1.txt" In_channel.input_all in
   String.split_on_char '\n' contents
 
-let rec row : string -> (int list) = function 
+let rec get_numbers : bool -> string -> (int list) = fun version line -> match line with 
   | "" -> []
-  | ch -> let remRow = row (String.sub ch 1 (String.length ch - 1)) in
-          let i = int_of_char (String.get ch 0) in 
-          if i >= 49 && i <= 57 then (i - 48) :: remRow else remRow
-
-let rec letter_row : string -> (int list) = function 
-  | "" -> []
-  | ch -> (if String.starts_with ~prefix:"one" ch then 1 :: letter_row (String.sub ch 2 (String.length ch - 2)) 
-          else if String.starts_with ~prefix:"two" ch then 2 :: letter_row (String.sub ch 2 (String.length ch - 2))
-          else if String.starts_with ~prefix:"three" ch then 3 :: letter_row (String.sub ch 4 (String.length ch - 4))
-          else if String.starts_with ~prefix:"four" ch then 4 :: letter_row (String.sub ch 4 (String.length ch - 4)) 
-          else if String.starts_with ~prefix:"five" ch then 5 :: letter_row (String.sub ch 3 (String.length ch - 3))
-          else if String.starts_with ~prefix:"six" ch then 6 :: letter_row (String.sub ch 3 (String.length ch - 3))
-          else if String.starts_with ~prefix:"seven" ch then 7 :: letter_row (String.sub ch 4 (String.length ch - 4))
-          else if String.starts_with ~prefix:"eight" ch then 8 :: letter_row (String.sub ch 4 (String.length ch - 4))
-          else if String.starts_with ~prefix:"nine" ch then 9 :: letter_row (String.sub ch 3 (String.length ch - 3))
-          else 
-            let remRow = letter_row (String.sub ch 1 (String.length ch - 1)) in
+  | ch -> if not version then (
+            let remRow = get_numbers version (String.sub ch 1 (String.length ch - 1)) in
             let i = int_of_char (String.get ch 0) in 
-            if i >= 49 && i <= 57 then (i - 48) :: remRow else remRow)  
+            if i >= 49 && i <= 57 then (i - 48) :: remRow else remRow)
+          else (if String.starts_with ~prefix:"one"        ch then 1 :: get_numbers version (String.sub ch 2 (String.length ch - 2))
+                else if String.starts_with ~prefix:"two"   ch then 2 :: get_numbers version (String.sub ch 2 (String.length ch - 2))
+                else if String.starts_with ~prefix:"three" ch then 3 :: get_numbers version (String.sub ch 4 (String.length ch - 4))
+                else if String.starts_with ~prefix:"four"  ch then 4 :: get_numbers version (String.sub ch 4 (String.length ch - 4))
+                else if String.starts_with ~prefix:"five"  ch then 5 :: get_numbers version (String.sub ch 3 (String.length ch - 3))
+                else if String.starts_with ~prefix:"six"   ch then 6 :: get_numbers version (String.sub ch 3 (String.length ch - 3))
+                else if String.starts_with ~prefix:"seven" ch then 7 :: get_numbers version (String.sub ch 4 (String.length ch - 4))
+                else if String.starts_with ~prefix:"eight" ch then 8 :: get_numbers version (String.sub ch 4 (String.length ch - 4))
+                else if String.starts_with ~prefix:"nine"  ch then 9 :: get_numbers version (String.sub ch 3 (String.length ch - 3))
+                else 
+                  let remRow = get_numbers version (String.sub ch 1 (String.length ch - 1)) in
+                  let i = int_of_char (String.get ch 0) in 
+                  if i >= 49 && i <= 57 then (i - 48) :: remRow else remRow)
 
-let rec numbers : string list -> (int list) list = function 
-  | [] -> []
-  | line :: lines -> row line :: numbers lines
+let rec numbers : string list -> (int list) list = List.map (get_numbers false)
 
-let rec letter_numbers : string list -> (int list) list = function 
-  | [] -> []
-  | line :: lines -> letter_row line :: letter_numbers lines
+let rec letter_numbers : string list -> (int list) list = List.map (get_numbers true)
 
-let rec values : (int list) list -> int list = function 
-  | [] -> []
-  | v :: vs -> ((10 * (List.nth v 0)) + (List.nth v (List.length v - 1))) :: values vs
+let rec values : (int list) list -> int list = List.map (fun v -> (10 * (List.nth v 0)) + (List.nth v (List.length v - 1)))
 
 let rec sum : int list -> int = function 
   | [] -> 0 
