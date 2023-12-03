78c37aa5c7da0f6fba8394041b9e541cd43321be
Seppe Lesschaeve <seppe.lesschaeve@student.kuleuven.be>
2023-12-03 23:39:44 +0100

    Finished day 3 + refactoring

--- day02/day2.ml
+++ day02/day2.ml
@@ -3,10 +3,10 @@ let lines =
   String.split_on_char '\n' contents
 
 let rec colorCount : string -> int list -> int list = 
-  fun color counts ->
-    let color = String.split_on_char ' ' color in
-    let count = int_of_string (List.nth color 1) in
-    let color = List.nth color 2 in
+  fun colorString counts ->
+    let countAndColor = String.split_on_char ' ' colorString in
+    let count = int_of_string (List.nth countAndColor 1) in
+    let color = List.nth countAndColor 2 in
     match color with
       | "red" -> [count; List.nth counts 1; List.nth counts 2]
       | "green" -> [List.nth counts 0; count; List.nth counts 2]
@@ -16,28 +16,22 @@ let rec colorCount : string -> int list -> int list =
 let rec colorCounts : string list -> int list -> (int * int * int) = 
   fun counts count -> match counts with
     | [] -> (List.nth count 0, List.nth count 1, List.nth count 2) 
-    | color :: colors -> colorCounts colors (colorCount color count)
-
-let count_cube : string list -> int * int * int =
-  fun cube_set -> colorCounts cube_set [0; 0; 0]
+    | colorString :: colors -> colorCounts colors (colorCount colorString count)
 
 let process_line : string -> (int * int * int) list =
   fun line -> let line = String.sub line (String.index line ':' + 1) (String.length line - String.index line ':' - 1) in
               let sets = String.split_on_char ';' line in
               let sets = List.map (fun x -> String.split_on_char ',' x) sets in
-              List.map count_cube sets
+              List.map (fun cubeSet -> colorCounts cubeSet [0;0;0]) sets
 
 let is_not_possible : (int * int * int) -> (int * int * int) -> bool = 
   fun (m1, m2, m3) (c1, c2, c3) -> c1 > m1 || c2 > m2 || c3 > m3 
 
-let rec validate : (int * int * int) -> (int * int * int) list -> bool -> bool = 
+let rec validate_game : (int * int * int) -> (int * int * int) list -> bool -> bool = 
   fun max_cube game possible -> match game with 
   | [] -> possible 
-  | one_set :: games -> let newPossible = if (is_not_possible max_cube one_set) then false else possible
-                        in validate max_cube games newPossible  
-
-let validate_game : (int * int * int) -> ((int * int * int) list) -> bool =
-  fun max_cube game -> validate max_cube game true 
+  | one_set :: games -> let newPossible = if (not possible || is_not_possible max_cube one_set) then false else possible
+                        in validate_game max_cube games newPossible  
 
 let rec min_cube_power : ((int * int * int) list) -> (int * int * int) -> int =
   fun colors (m1, m2, m3) -> match colors with
@@ -50,7 +44,7 @@ let main () =
   let id_sum = ref 0 in
   let min_power_sum = ref 0 in
     List.iteri (fun idx line ->
-      if validate_game max_possible line then id_sum := !id_sum + idx + 1;
+      if validate_game max_possible line true then id_sum := !id_sum + idx + 1;
       min_power_sum := !min_power_sum + min_cube_power line (1, 1, 1)) lines;
   print_int !id_sum;
   print_newline ();
---
+++ day03/day3.ml
@@ -0,0 +1,66 @@
+let lines =
+  let contents = In_channel.with_open_bin "day3.txt" In_channel.input_all in
+  String.split_on_char '\n' contents
+
+type number = { value: int; y: int; xmin: int; xmax: int; }
+
+type symbol = { text: char; x: int; y: int; }
+
+type grid = { numbers: number list; symbols: symbol list; }
+
+let fold_grid s1 s2 = { numbers = List.append s1.numbers s2.numbers; symbols = List.append s1.symbols s2.symbols }
+
+let adj symbol number = number.xmin - 1 <= symbol.x && symbol.x <= number.xmax + 1 && abs (number.y - symbol.y) <= 1
+
+let rec of_char_list : char list -> string = function 
+  | [] -> ""
+  | c :: cs -> (String.make 1 c) ^ of_char_list cs
+
+let rec to_chars : string -> (char list) = function 
+  | "" -> []
+  | ch -> (String.get ch 0) :: to_chars (String.sub ch 1 (String.length ch - 1))
+
+let rec to_char_list : string list -> (char list) list = function 
+  | [] -> []
+  | str :: strs -> (to_chars str) :: to_char_list strs
+
+let parse (input: string list) : grid =
+  let rec aux input num xmax y numbers symbols =
+    let xmax = xmax + 1 in
+    match input with
+    | ('0'..'9' as i1) :: ('0'..'9' as i2) :: tl -> aux (i2 :: tl) (i1 :: num) xmax y numbers symbols
+    | ('0'..'9' as i1) :: tl -> 
+        (
+          let value = (i1 :: num) |> List.rev |> of_char_list |> int_of_string in
+          let xmin = xmax - (List.length num) in
+          let new_num = {value; y; xmin; xmax} in
+          aux tl [] (xmax) y (new_num :: numbers) symbols
+        )
+    | '.' :: tl -> aux tl num xmax y numbers symbols
+    | text :: tl -> aux tl num xmax y numbers ({x = xmax; y; text} :: symbols)
+    | [] -> {numbers; symbols}
+  in
+  let rows = List.mapi (fun y -> fun row -> aux row [] 0 y [] []) (to_char_list input) in
+  List.fold_left (fold_grid) ({numbers = []; symbols = []}) rows
+
+let p1_calc grid : int = grid.numbers |> List.filter (fun num -> grid.symbols |> List.exists (fun sym -> adj sym num)) |> List.map (fun num -> num.value) |> List.fold_left (+) 0
+
+let p2_calc grid : int = grid.symbols |> List.filter_map 
+    (
+      fun sym -> 
+      let adj_nums = grid.numbers |> List.filter (adj sym) in
+      match sym.text, adj_nums with
+        | '*', { value = val1; _} :: {value = val2; _} :: []  -> Some (val1 * val2)
+        | _ -> None
+    ) |> List.fold_left (+) 0
+
+let main = 
+  begin
+    let grid = lines |> parse in
+    print_int (p1_calc grid);
+    print_newline ();
+    print_int (p2_calc grid);
+    print_newline ();
+  end
+
+let () = main
